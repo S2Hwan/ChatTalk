fd3797de125769f222ae65ef92dcd99903b3f8d9
s2h <ssh92kr0410@naver.com>
2018-05-10 20:25:51 +0900

    버튼, statusbar 생성 및 색 변경
    firebase, snpakit 이용

--- ChatTalk/LoginViewController.swift
+++ ChatTalk/LoginViewController.swift
@@ -7,29 +7,35 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    let remoteConfig = RemoteConfig.remoteConfig()
+    
+    var color : String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+         // statusbar 설정
+        let statusBar = UIView()
+        self.view.addSubview(statusBar)
+        statusBar.snp.makeConstraints { (make) in
+            make.right.top.left.equalTo(self.view)
+            make.height.equalTo(20)
+        }
+        
+        color = remoteConfig["splash_background"].stringValue
+        
+        statusBar.backgroundColor = UIColor(hex: color)
+        loginButton.backgroundColor = UIColor(hex: color)
+        registerButton.backgroundColor = UIColor(hex: color)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
